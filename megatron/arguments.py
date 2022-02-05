ec6a3d13463f1a610040b7275574028c62694443
Conglong Li <conglong.li@gmail.com>
2022-02-04 23:06:51 -0800

    eval harness support from big science workshop

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -480,6 +480,9 @@ def _add_training_args(parser):
                        help='Disable pipeline parallelism')
     group.add_argument('--use-tutel', action='store_true',
                        help='Use Tutel optimization for MoE')
+    group.add_argument('--inference', action='store_true',
+                       help='Very basic inference mode: not allocating optim/lr - requires ZERO_STAGE=0')
+
     return parser
 
 
