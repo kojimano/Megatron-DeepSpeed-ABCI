3cd12c1f804373bc8bf7d5857976e91ca5d61594
Conglong Li <conglong.li@gmail.com>
2022-02-28 17:35:37 -0800

    add doc comment

--- megatron/training.py
+++ megatron/training.py
@@ -625,6 +625,8 @@ def training_log(loss_dict, total_loss_dict, learning_rate, iteration,
                          normalizer=total_iterations)
 
     if iteration % args.tensorboard_log_interval == 0:
+        # This logging write various optimizer states to tensorboard. This
+        # feature may consume extra GPU memory thus is set at false by default.
         if args.log_optimizer_states_to_tensorboard and optimizer is not None:
             opt_stats = [0.0] * 8
             opt_stats_2 = [0.0] * 4
