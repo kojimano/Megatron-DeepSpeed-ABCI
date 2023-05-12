eb9d43440f2a48128bc36d3ab854bd58be30fc39
kojimano <nk654>
2023-05-13 00:57:25 +0900

    Updating debug scripts.

---
+++ abci/jobs/submit_pretrain_gpt_345m_4gpu_debug_abci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -l rt_F=4
+#$ -l h_rt=5:00:00
+#$ -j y
+#$ -cwd
+#$ -o /bb/grandchallenge/gaf51090/job_outputs/pretrain_gpt_345m_4gpu_debug_abci.$JOB_ID
+
+source /etc/profile.d/modules.sh
+module load python/3.10/3.10.10 cuda/11.8/11.8.0 cudnn/8.6/8.6.0 nccl/2.16/2.16.2-1
+module unload intel-mpi/2021.8
+module load hpcx/2.12
+source /home/acf15317dw/megatron-deepspeed/bin/activate
+cd /home/acf15317dw/Megatron-DeepSpeed-ABCI
+sh abci/shells/pretrain_gpt_345m_4gpu_debug_abci.sh
--- abci/shells/pretrain_gpt_345m_4gpu_debug_abci.sh
+++ abci/shells/pretrain_gpt_345m_4gpu_debug_abci.sh
@@ -13,8 +13,8 @@ cat $SGE_JOB_HOSTLIST > $HOSTFILE_NAME
 # Data path
 DATA_PATH='
        5 /bb/grandchallenge/gaf51090/datasets/aozora_books/binarized/abeja/aozora_books_text_document
-       1 /bb/grandchallenge/gaf51090/datasets/wikipedia/binarized/abeja/en_wiki_text_document
-       3 /bb/grandchallenge/gaf51090/datasets/wikipedia/binarized/abeja/ja_wiki_text_document
+       35 /bb/grandchallenge/gaf51090/datasets/wikipedia/binarized/abeja/en_wiki_text_document
+       60 /bb/grandchallenge/gaf51090/datasets/wikipedia/binarized/abeja/ja_wiki_text_document
 '
 
 # Model size
@@ -23,8 +23,11 @@ HIDDEN_SIZE=1024
 NUM_ATTENTION_HEADS=16
 
 # Other experimental parameters
-CHECKPOINT_PATH=/bb/grandchallenge/gaf51090/checkpoints/345m_4gpu_debug
-TENSORBOARD_PATH=/bb/grandchallenge/gaf51090/logs/345m_4gpu_debug
+EXP_NAME=345m_4gpu_debug_training
+CHECKPOINT_PATH=/bb/grandchallenge/gaf51090/checkpoints/${EXP_NAME}
+TENSORBOARD_PATH=/bb/grandchallenge/gaf51090/logs/${EXP_NAME}
+WANDB_NAME=${EXP_NAME}
+export WANDB_DIR='/bb/grandchallenge/gaf51090/wandb'
 VOCAB_FILE=''
 
 mpirun -np $WORLD_SIZE -npernode $GPUS_PER_NODE --hostfile $HOSTFILE_NAME python pretrain_gpt.py \
@@ -33,15 +36,15 @@ mpirun -np $WORLD_SIZE -npernode $GPUS_PER_NODE --hostfile $HOSTFILE_NAME python
        --num-layers $NUM_LAYERS \
        --hidden-size $HIDDEN_SIZE \
        --num-attention-heads $NUM_ATTENTION_HEADS \
-       --micro-batch-size 1 \
-       --global-batch-size 80 \
+       --micro-batch-size 4 \
+       --global-batch-size 1344 \
        --seq-length 2048 \
        --max-position-embeddings 2048 \
        --save $CHECKPOINT_PATH \
        --load $CHECKPOINT_PATH \
        --tensorboard-dir $TENSORBOARD_PATH \
-       --train-samples 300000 \
-       --lr-decay-samples 300000 \
+       --wandb-name $WANDB_NAME \
+       --train-iters 53854 \
        --data-path $DATA_PATH \
        --tokenizer-type AbejaJapaneseGPT2Tokenizer \
        --data-impl mmap \
@@ -58,7 +61,7 @@ mpirun -np $WORLD_SIZE -npernode $GPUS_PER_NODE --hostfile $HOSTFILE_NAME python
        --hidden-dropout 0 \
        --attention-dropout 0 \
        --weight-decay 0.1 \
-       --log-interval 10 \
+       --log-interval 1 \
        --save-interval 100 \
        --eval-interval 100 \
        --eval-iters 10 \
@@ -66,8 +69,8 @@ mpirun -np $WORLD_SIZE -npernode $GPUS_PER_NODE --hostfile $HOSTFILE_NAME python
        --checkpoint-activations \
        --distribute-checkpointed-activations \
        --no-scatter-gather-tensors-in-pipeline \
-       --use-mpi
-
+       --use-mpi \
+       --log-timers-to-tensorboard
 
 rm $HOSTFILE_NAME
 
