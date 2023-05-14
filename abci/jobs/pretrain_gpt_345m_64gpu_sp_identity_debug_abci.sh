a337270ef67f7256995b08210a4312146c9d3fdd
kojimano <nk654>
2023-05-15 08:54:27 +0900

    More scripts.

--- abci/jobs/pretrain_gpt_345m_64gpu_sp_identity_debug_abci.sh
+++ abci/jobs/pretrain_gpt_345m_64gpu_sp_identity_debug_abci.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#$ -l rt_F=4
-#$ -l h_rt=5:00:00
+#$ -l rt_F=16
+#$ -l h_rt=10:00:00
 #$ -j y
 #$ -cwd
-#$ -o /bb/grandchallenge/gaf51090/job_outputs/pretrain_gpt_345m_4gpu_debug_abci.$JOB_ID
+#$ -o /bb/grandchallenge/gaf51090/job_outputs/pretrain_gpt_345m_64gpu_sp_identity_debug_abci.$JOB_ID
 
 source /etc/profile.d/modules.sh
 module load python/3.10/3.10.10 cuda/11.8/11.8.0 cudnn/8.6/8.6.0 nccl/2.16/2.16.2-1
@@ -11,4 +11,4 @@ module unload intel-mpi/2021.8
 module load hpcx/2.12
 source /home/acf15317dw/megatron-deepspeed/bin/activate
 cd /home/acf15317dw/Megatron-DeepSpeed-ABCI
-sh abci/shells/pretrain_gpt_345m_4gpu_debug_abci.sh
+sh abci/shells/pretrain_gpt_345m_64gpu_sp_identity_debug_abci.sh
